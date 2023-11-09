2dfaea5c26f94471f67e628498f49efe2ade42d3
Selçuk İleri <selcukileri98@gmail.com>
2023-11-09 15:51:53 +0300

    Initial Commit

---
+++ NetworkInterchangable/NetworkInterchangableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangableApp.swift
+//  NetworkInterchangable
+//
+//  Created by Selçuk İleri on 9.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
